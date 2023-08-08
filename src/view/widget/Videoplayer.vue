0cd27f843bf2d882b39536381ba8ad7c0e776a39
Matin-Laptop <matink1490@gmail.com>
2023-08-08 11:29:56 +0330

    Handle video player

---
+++ src/view/widget/Videoplayer.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-card>
+        <v-responsive :aspect-ratio="16/9">
+            <video
+                    style="width: 100%; height: 100%"
+                    ref="videoPlayer" class="video-js">
+            </video>
+        </v-responsive>
+    </v-card>
+
+</template>
+
+<script>
+import videojs from 'video.js';
+
+export default {
+    name: "Videoplayer",
+    data() {
+        return {
+            player: null
+        }
+    },
+    mounted() {
+        this.player = videojs(this.$refs.videoPlayer, this.options, () => {
+            this.player.log('onPlayerReady', this);
+        });
+    },
+    beforeDestroy() {
+        if (this.player) {
+            this.player.dispose();
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
