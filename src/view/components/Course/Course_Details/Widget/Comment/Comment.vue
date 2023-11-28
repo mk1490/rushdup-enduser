8f8a8675a44bc184c8b0308a00db06aacd330ac3
Server <matink1490@gmail.com>
2023-11-29 03:09:14 +0330

    Handle comment area.

--- src/view/components/Course/Course_Details/Widget/Comment/Comment.vue
+++ src/view/components/Course/Course_Details/Widget/Comment/Comment.vue
@@ -3,26 +3,25 @@
     <div id="comments" class="comments-area">
         <div class="comments-wrap">
             <h2 class="comments-title">
-                نظر <span class="count">({{commentItems.length}})</span></h2>
+                نظر <span class="count">({{ commentItems.length }})</span></h2>
             <div class="comment-list-wrap">
 
                 <ol class="comment-list">
                     <li
                             v-for="item in commentItems"
-                            class="comment byuser comment-author-admin bypostauthor even thread-even depth-1"
-                            id="li-comment-10">
+                            class="comment byuser comment-author-admin bypostauthor even thread-even depth-1">
                         <div id="comment-10" class="comment-wrap">
                             <div class="comment-author vcard">
-                                <img alt=""
-                                     src="@/assets/images/avatar.png"
-                                     class="avatar avatar-70 photo" height="70" width="70" loading="lazy"
-                                     decoding="async"></div>
+                                <img
+                                        src="@/assets/images/avatar.png"
+                                        class="avatar avatar-70 photo" height="70" width="70" loading="lazy"
+                                        decoding="async"></div>
                             <div class="comment-content">
                                 <div class="meta">
                                     <h6 class="fn">
                                         <a class="url"
                                            rel="ugc">
-                                            {{item.family + ' ' + item.name}}
+                                            {{ item.family + ' ' + item.name }}
                                         </a>
                                     </h6>
                                 </div>
@@ -38,49 +37,39 @@
                                     </div>
 
                                     <div class="comment-actions">
-                                        <a rel="nofollow" class="comment-reply-link"
-                                           href="http://localhost:9090/edumall/how-working-from-home-is-transforming-learning/?replytocom=10#respond"
-                                           data-commentid="10" data-postid="92" data-belowelement="comment-10"
-                                           data-respondelement="respond" data-replyto="پاسخ به مدیرسایت"
-                                           aria-label="پاسخ به مدیرسایت">پاسخ</a> <a class="comment-edit-link"
-                                                                                     href="http://localhost:9090/edumall/wp-admin/comment.php?action=editcomment&amp;c=10">ویرایش</a>
+                                        <a
+                                                href="javascript:void(0)"
+                                                @click="handleResponse(item)"
+                                                class="comment-reply-link"
+                                                aria-label="پاسخ به مدیرسایت">
+                                            پاسخ
+                                        </a>
+                                        <a
+                                                v-if="false"
+                                                class="comment-edit-link"
+                                                href="javascript:void(0)">ویرایش</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <comment-form-area
+                                v-if="responseToCommentId"
+                                :response-mode="true"
+                                :responser-title="item.family + ' ' + item.name "
+                                @on-cancel-response="responseToCommentId = null"
+                        />
                     </li>
+
                 </ol>
 
 
             </div>
         </div>
 
-        <div class="comment-form-wrap">
-            <div id="respond" class="comment-respond">
-                <h3 id="reply-title" class="comment-reply-title">دیدگاه خود را اینجا قرار دهید <small><a rel="nofollow"
-                                                                                                         id="cancel-comment-reply-link"
-                                                                                                         href="/edumall/how-working-from-home-is-transforming-learning/#respond"
-                                                                                                         style="display:none;">لغو
-                    پاسخ</a></small></h3>
-                <form action="http://localhost:9090/edumall/wp-comments-post.php" method="post" id="commentform"
-                      class="comment-form" novalidate=""><p class="logged-in-as">به عنوان مدیرسایت وارد شده‌اید. <a
-                        href="http://localhost:9090/edumall/members/admin/profile/edit/">نمایهٔ خود را ویرایش نمایید</a>.
-                    <a href="http://localhost:9090/edumall/wp-login.php?action=logout&amp;redirect_to=http%3A%2F%2Flocalhost%3A9090%2Fedumall%2Fhow-working-from-home-is-transforming-learning%2F&amp;_wpnonce=606c650831"
-                       data-no-instant="">بیرون رفتن؟</a> <span class="required-field-message">بخش‌های موردنیاز علامت‌گذاری شده‌اند <span
-                            class="required">*</span></span></p>
-                    <div class="row">
-                        <div class="col-md-12 comment-form-comment"><textarea id="comment" placeholder="نظر شما"
-                                                                              name="comment"
-                                                                              aria-required="true"></textarea></div>
-                    </div>
-                    <p class="form-submit"><input name="submit" type="submit" id="submit" class="submit" value="ارسال">
-                        <input type="hidden" name="comment_post_ID" value="92" id="comment_post_ID">
-                        <input type="hidden" name="comment_parent" id="comment_parent" value="0">
-                    </p><input type="hidden" id="_wp_unfiltered_html_comment_disabled"
-                               name="_wp_unfiltered_html_comment" value="9c8c4a5bf5">
-                </form>
-            </div>
-        </div>
+        <comment-form-area
+                v-if="!responseToCommentId"
+                :response-mode="false"
+        />
 
     </div>
   <!--    <div class="tutor-single-course-segment tutor-course-reviews-wrap">-->
@@ -106,18 +95,36 @@
 
 <script>
 import CommentModal from "@/view/components/Course/Course_Details/Widget/Comment/CommentModal.vue";
+import CommentFormArea from "@/view/components/Course/Course_Details/Widget/Comment/CommentFormArea.vue";
 
 export default {
     name: "Comment",
-    components: {CommentModal},
+    components: {CommentFormArea, CommentModal},
     props: {
         commentItems: Array,
+    },
+    data() {
+        return {
+            model: {
+                content: null,
+            },
+            responseToCommentId: null,
+        }
     },
     emits: ['showModalClick'],
     methods: {
         showCommentModal() {
             this.$emit('showModalClick')
         },
+        handleResponse(item) {
+            this.responseToCommentId = item.commentId;
+        },
+        async sendComment() {
+            const [err, data] = await this.to(this.http.post(`/comment/${this.courseId}`, this.model));
+            if (!err) {
+
+            }
+        }
     }
 }
 </script>
---
+++ src/view/components/Course/Course_Details/Widget/Comment/CommentFormArea.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="comment-form-wrap">
+        <div id="respond" class="comment-respond">
+            <h3 id="reply-title" class="comment-reply-title">دیدگاه خود را اینجا قرار دهید <small>
+                <a rel="nofollow"
+                   id="cancel-comment-reply-link"
+                   style="display:none;">لغو
+                    پاسخ</a></small></h3>
+
+
+            <div class="comment-form">
+                <p class="logged-in-as">
+                        <span
+                                v-if="responseMode"
+                                class="required-field-message">
+                            پاسخ به {{ responserTitle }}
+                        </span>
+                    <small v-if="responseMode">
+                        <a @click="$emit('onCancelResponse')">
+                            لغو پاسخ
+                        </a>
+                    </small>
+                </p>
+                <div class="row">
+                    <div class="col-md-12 comment-form-comment">
+                            <textarea
+                                    v-model="content"
+                                    id="comment"
+                                    placeholder="نظر شما">
+                            </textarea>
+                    </div>
+                </div>
+            </div>
+            <v-btn
+                    @click="$emit('onSubmit', content)"
+                    class="mt-2"
+                    color="primary"
+                    width="130"
+                    height="50">
+                ارسال
+            </v-btn>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CommentFormArea",
+    emits: ['onSubmit', 'onCancelResponse'],
+    props: {
+        responseMode: Boolean,
+        responserTitle: String,
+    },
+    data() {
+        return {
+            content: null,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
